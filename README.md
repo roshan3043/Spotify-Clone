4c553151c99ca2ad4080090db1a315ae3fcb41df
Roshan <roshan047007@gmail.com>
2024-06-15 23:18:17 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,61 @@
+# 40π Music Player Website  &nbsp; [![](https://img.shields.io/badge/-HTML5-darkred?style=flat&logo=HTML5&logoColor=white)&nbsp; ![](https://img.shields.io/badge/-CSS3-blue?style=flat&logo=CSS3&logoColor=white)&nbsp;![](https://img.shields.io/badge/-JavaScript-gold?style=flat&logo=JavaScript&logoColor=black)](https://github.com/AnshSinghSonkhia/40Pie-Music-Player-Website/blob/main/README.md)&nbsp;
+
+![](https://spotify-cards.fossian.com/api?name=Gangsta-Paradise&color=09516f)
+
+![](https://i.imgur.com/waxVImv.png)
+
+<b>Let's Connect: [LinkedIn](https://www.linkedin.com/in/anshsinghsonkhia/)</b>
+
+# Tech-Stack Used..
+[<img src="https://github.com/devicons/devicon/blob/master/icons/html5/html5-original-wordmark.svg" title="HTML" alt="HTML" width="60" height="60"/>&nbsp;
+<img src="https://github.com/devicons/devicon/blob/master/icons/css3/css3-original-wordmark.svg" title="css3" alt="css3" width="60" height="60"/>&nbsp;
+<img src="https://github.com/devicons/devicon/blob/master/icons/javascript/javascript-original.svg" title="JS" alt="JS" width="60" height="60"/>&nbsp;](https://github.com/AnshSinghSonkhia/40Pie-Music-Player-Website/blob/main/README.md)
+
+![](https://i.imgur.com/waxVImv.png)
+
+
+## Thanks to all Contributors ✨
+
+Thanks Goes To These Wonderful People ❤ <!--([emoji key](https://allcontributors.org/docs/en/emoji-key)):-->
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tbody>
+    <tr>
+      <td align="center"><a href="https://github.com/AnshSinghSonkhia"><img src="https://avatars.githubusercontent.com/u/110414565?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Ansh Singh Sonkhia</b></sub><br> :computer: </a></td>
+      <td align="center"><a href="https://github.com/devhvrshu"><img src="https://avatars.githubusercontent.com/u/108367606?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Harsh Mishra</b></sub><br> :computer: </a></td>
+      <td align="center"><a href="https://github.com/Shubhcs01"><img src="https://avatars.githubusercontent.com/u/81324145?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Shubham Gupta</b></sub><br> :computer: </a></td>
+      <td align="center"><a href="https://github.com/vaibhavmishra4g"><img src="https://avatars.githubusercontent.com/u/89067345?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Vaibhav Mishra</b></sub><br> :computer: </a></td>
+      <td align="center"><a href="https://github.com/lyallemma"><img src="https://avatars.githubusercontent.com/u/25173082?v=4?s=100" width="100px;" alt=""/><br /><sub><b>lyallemma</b></sub><br> :computer: </a></td>
+      <td align="center"><a href="https://github.com/Kingsman44"><img src="https://avatars.githubusercontent.com/u/58323485?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Shivan Singh</b></sub><br> :computer: </a></td>
+     </tr>
+  </tbody>
+</table>
+
+
+
+![](https://i.imgur.com/waxVImv.png)
+
+
+<!-- 
+## Thanks to all contributors ❤
+
+ <a href = "https://github.com/AnshSinghSonkhia/40Pie-Music-Player-Website/graphs/contributors">
+   <img src = "https://contrib.rocks/image?repo=AnshSinghSonkhia/40Pie-Music-Player-Website"/>
+ </a> ...
+ -->
+
+## License
+
+Have a look at the [LICENSE file](./LICENSE) for details
+
+
+<div align="center">
+
+![](https://i.imgur.com/waxVImv.png)
+
+<h3> Show Some <img src="https://github.com/AnshSinghSonkhia/AnshSinghSonkhia/blob/main/icons/love.png" title="Love" alt="Love" width="20" height="20"/> by Starring Some of My Repos! </h3>
+
+</div>
---
+++ script.js
@@ -0,0 +1,322 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio("songs/1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+// let songItemPlay = document.getElementsByClassName("songItemPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+let masterSongName = document.getElementById("masterSongName");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
+let currTime = document.querySelector(".current-time");
+let totalDuration = document.querySelector(".total-duration");
+
+let songs = [
+  {
+    songName: "Railin Oligal",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "Gulabi Sadi",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/2.jpg",
+  },
+  {
+    songName: "Dusk Till Dawn",
+    filePath: "songs/3.mp3",
+    coverPath: "covers/3.jpg",
+  },
+  {
+    songName: "Levitating",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/4.jfif",
+  },
+  {
+    songName: "No-Lie",
+    filePath: "songs/5.mp3",
+    coverPath: "covers/5.jfif",
+  },
+  {
+    songName: "One-Kiss",
+    filePath: "songs/6.mp3",
+    coverPath: "covers/6.jfif",
+  },
+  {
+    songName: "Cielo Huma Huma",
+    filePath: "songs/7.mp3",
+    coverPath: "covers/7.jpg",
+  },
+  {
+    songName: "Janji - Heroes Tonight",
+    filePath: "songs/8.mp3",
+    coverPath: "covers/8.jpg",
+  },
+  {
+    songName: "Despacito",
+    filePath: "songs/9.mp3",
+    coverPath: "covers/9.jpg",
+  },
+  {
+    songName: "Shape of you",
+    filePath: "songs/10.mp3",
+    coverPath: "covers/10.jpg",
+  },
+  {
+    songName: "Tsunami",
+    filePath: "songs/11.mp3",
+    coverPath: "covers/11.jpg",
+  },
+  {
+    songName: "Nero-Promises",
+    filePath:"songs/12.mp3",
+    coverPath:"covers/12.jpeg"
+    
+  },
+  {
+    songName: "Baahubali",
+    filePath: "songs/13.mp3",
+    coverPath: "covers/13.jpg",
+  },
+  {
+    songName: "Low",
+    filePath: "songs/14.mp3",
+    coverPath: "covers/14.jpg",
+  },
+  {
+    songName: "Final Gates",
+    filePath: "songs/15.mp3",
+    coverPath: "covers/15.jpg",
+  },
+  {
+    songName: "KGF Ringtone",
+    filePath: "songs/16.mp3",
+    coverPath: "covers/16.jpg",
+  },
+];
+//Making a map to store songName and there index value
+const songIndexValPair = new Map();
+songItems.forEach((element, i) => {
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerHTML = songs[i].songName;
+  songIndexValPair.set(songs[i].songName,i);
+});
+
+//audioElement.play();
+
+// Pause all song icon
+function pauseall() {
+  songItems.forEach((elements, i) => {
+    var element1 = document.getElementById(i);
+      element1.classList.remove("fa-circle-pause");
+      element1.classList.add("fa-circle-play");
+  });
+}
+
+// Start playing song icon
+function startplay() {
+  newsong = document.getElementById(songIndex);
+  newsong.classList.remove("fa-circle-play");
+  newsong.classList.add("fa-circle-pause");
+}
+
+// Stop playing song icon
+function stopplay() {
+  newsong = document.getElementById(songIndex);
+  newsong.classList.remove("fa-circle-pause");
+  newsong.classList.add("fa-circle-play");
+}
+
+// Handle Play/Pause Click
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
+    startplay();
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-circle-pause");
+    masterPlay.classList.add("fa-circle-play");
+    gif.style.opacity = 0;
+    stopplay();
+  }
+});
+
+// Listen to Events
+audioElement.addEventListener("timeupdate", () => {
+  //   console.log("timeUpdate");
+  //Update Seekbar
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  //console.log(progress);
+  myProgressBar.value = progress;
+
+  // Calculate current time left and total duration
+  let currentMinutes = Math.floor(audioElement.currentTime / 60);
+  let currentSeconds = Math.floor(
+    audioElement.currentTime - currentMinutes * 60
+  );
+  let durationMinutes = Math.floor(audioElement.duration / 60);
+  let durationSeconds = Math.floor(
+    audioElement.duration - durationMinutes * 60
+  );
+
+  if (currentSeconds < 10) {
+    currentSeconds = "0" + currentSeconds;
+  }
+  if (durationSeconds < 10) {
+    durationSeconds = "0" + durationSeconds;
+  }
+  if (currentMinutes < 10) {
+    currentMinutes = "0" + currentMinutes;
+  }
+  if (durationMinutes < 10) {
+    durationMinutes = "0" + durationMinutes;
+  }
+
+  // Updated duration
+  currTime.textContent = currentMinutes + ":" + currentSeconds;
+  totalDuration.textContent = durationMinutes + ":" + durationSeconds;
+
+});
+
+myProgressBar.addEventListener("change", () => {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+  (element) => {
+    element.addEventListener("click", (e) => {
+      //   console.log(e.target);
+      songIndex = parseInt(e.target.id);
+      pauseall();
+      //e.target.classList.remove("fa-circle-play");
+      e.target.classList.add("fa-circle-pause");
+      audioElement.src = `songs/${songIndex + 1}.mp3`;
+      masterSongName.innerText = songs[songIndex].songName;
+      audioElement.currentTime = 0;
+      audioElement.play();
+      gif.style.opacity = 1;
+      masterPlay.classList.remove("fa-circle-play");
+      masterPlay.classList.add("fa-circle-pause");
+    });
+  }
+);
+
+document.getElementById("next").addEventListener("click", () => {
+  if (songIndex >= 14) {
+    songIndex = 0;
+  } else {
+    songIndex += 1;
+  }
+  //Adding removePlaying function to remove highlight from previous song
+  removingPlaying();
+  //Adding addPlaying to add highlight to the currently playing song.
+  addPlaying(songIndex);
+  audioElement.src = `songs/${songIndex + 1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  pauseall();
+  startplay();
+  masterPlay.classList.remove("fa-circle-play");
+  masterPlay.classList.add("fa-circle-pause");
+});
+
+document.getElementById("previous").addEventListener("click", () => {
+  if (songIndex <= 0) {
+    songIndex = 0;
+  } else {
+    songIndex -= 1;
+  }
+  //Adding removePlaying function to remove highlight from previous song
+  removingPlaying();
+  //Adding addPlaying to add highlight to the currently playing song.
+  addPlaying(songIndex);
+  audioElement.src = `songs/${songIndex + 1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  pauseall();
+  startplay();
+  masterPlay.classList.remove("fa-circle-play");
+  masterPlay.classList.add("fa-circle-pause");
+});
+
+audioElement.onended = function() {
+  // change the song index to the index of next song, if the song is the last one in the playlist then the next should be the first one
+  if (songIndex >= 14) 
+    songIndex = 0;
+  else 
+    songIndex += 1;
+  //Adding removePlaying function to remove highlight from previous song
+  removingPlaying();
+  //Adding addPlaying to add highlight to the currently playing song.
+  addPlaying(songIndex);
+  this.src = `songs/${songIndex + 1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  this.currentTime = 0;
+  // the audio player stops after playing each song, so after changing the src just launch the player
+  this.play();
+  pauseall();
+  startplay();
+  masterPlay.classList.remove("fa-circle-play");
+  masterPlay.classList.add("fa-circle-pause");
+}
+
+//Implementing responsive behaviour of songs container.
+//Selecting tags with songItem class.
+const songItem = document.querySelectorAll('.songItem');
+
+//Creating a function to add playing class which will highlight the playing song.
+const addPlaying = function (i) {
+  // console.log(songItem[0]);
+  songItem.forEach(function (val) {
+    // console.log(val.id)
+    if(val.id === `songItem${i}`)
+    {
+      val.classList.add('playing')
+    }
+  })
+}
+
+//Creating a function to remove playing class whenever other music starts playing.
+const removingPlaying = function(){
+  songItem.forEach(function(val){
+  // console.log(val.classList.value);
+  if(val.classList.value==='songItem playing'){
+    val.classList.remove('playing');
+  }
+
+})
+}
+
+//Code for implemenntation of clicking on the song in songs container and then playing it from there. 
+songItem.forEach(function(val){
+  val.addEventListener('click',function(e){
+    removingPlaying();
+    pauseall();
+    currentSong = e.path[0].innerHTML;
+    if(e.path[1].classList.value==='songItem'){
+      e.path[1].classList.add('playing');
+      songs.forEach(function(e){
+        if(currentSong===e.songName){
+          songIndex = songIndexValPair.get(currentSong);
+          songFilePath = songs[songIndex].filePath;
+          audioElement.src = songFilePath;
+          audioElement.play();
+          masterSongName.innerText = songs[songIndex].songName;
+          startplay();
+          masterPlay.classList.remove("fa-circle-play");
+          masterPlay.classList.add("fa-circle-pause");
+          gif.style.opacity = 1;
+        }
+      })
+    }
+
+  })
+})
+
